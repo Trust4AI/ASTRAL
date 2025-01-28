a7a7602d3aeaa63ef12f731605c5284619979653
mukerejeta <62596298+mukerejeta@users.noreply.github.com>
2025-01-28 15:04:05 +0100

    Add files via upload

---
+++ settings_empty.py
@@ -0,0 +1,6 @@
+API_KEY = 'null'                                                         # your OpenAI API KEY
+TAVILY_KEY = 'null'                                                      # Your Tavily Search API KEY
+address = 'null'                                                         # Your Ollama address and port   
+assistant_id_ASTRAL_RAG = 'null'                                         # Your ASTRAL_RAG assistant ID 
+assistant_id_ASTRAL_RAG_FS_TS = 'null'                                   # Your ASTRAL_RAG_FS_TS assistant ID 
+
